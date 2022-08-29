6fb1dcda22e9aa97ec2a6d5e3a0f433f5ef99c52
niki.hrovatin <niki.hrovatin@innorenew.eu>
2022-08-29 16:07:59 +0200

    template configuration comments

--- configuration_template.sh
+++ configuration_template.sh
@@ -16,9 +16,9 @@
 #
 ####
 
-
+#Each sensor must have a different host name
 SENSOR_HOSTNAME=my-name
-
+#Each sensors share the same username
 USER_ON_RASPI=pi
 
 
@@ -30,21 +30,27 @@ USER_ON_RASPI=pi
 #
 ####
 
-#delta in seconds between measurments
+#delta in seconds between taken measurments
 MEASUREMENT_DELTA=60
 #number of raw sensor lines to store in log (43200 = 4weeks) 
 SENSOR_LINES=43200 
+#Name of the influxdb bucket on the server where IAQ data will be stored
 INFLUX_IAQ_BUCKET=iaq
-#Influx setup
+#Server address 
 INFLUX_URL=
+#Organization name
 INFLUX_ORG=
+#Token for writing to INFLUX_IAQ_BUCKET and INFLUX_LOG_BUCKET
 INFLUX_TOKEN=
+#Elapsed time to delete a sent request in milliseconds
 INFLUX_TIMEOUT=20000
+#Name of the influxdb bucket on the server where logs will be stored
 INFLUX_LOG_BUCKET=rpi_stats
-#sensor identification
+#Same as the SENSOR_HOSTNAME
 SENSOR_ID=${SENSOR_HOSTNAME}
+#Identifier of the building in which the sensor will be placed
 BUILDING=my-home
-#local DB setup
+#Local DB setup, no need to change
 LOCAL_DB_HOST=host.docker.internal
 LOCAL_DB_DATABASE=local-aq
 LOCAL_DB_USERNAME=innorenew
@@ -63,7 +69,7 @@ SERVER_PORT=
 RASPI_PORT=
 #user on the server, that will be used to create the SSH tunnel
 SENSOR_USER=
-#IR server ip
+#Server ip
 SERVER_IP=
 
 
@@ -74,8 +80,11 @@ SERVER_IP=
 #
 ####
 
+#Name of the influxdb bucket on the server where sensor stats will be stored
 TELEGRAF_BUCKET=rpi_stats
+#Token placeholder that will be replaced later by the token
 TELEGRAF_TOKEN_PLACEHOLDER='$INFLUX_TOKEN'
+#The telegraf token to write data on the TELEGRAF_BUCKET
 TELEGRAF_TOKEN=
 
 
@@ -86,6 +95,6 @@ TELEGRAF_TOKEN=
 #
 ####
 
-
+#No need to change, just point at where SSH_KEYS are located
 SSH_PUBLIC=./ssh_keys/id_rsa.pub
 SSH_PRIVATE=./ssh_keys/id_rsa
