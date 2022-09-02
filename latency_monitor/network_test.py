b9a60d4eb5950d83f5924412072894ac99a035dd
niki.hrovatin <niki.hrovatin@innorenew.eu>
2022-09-02 12:03:31 +0200

    unified

--- latency_monitor/network_test.py
+++ latency_monitor/network_test.py
@@ -34,21 +34,22 @@ class NetworkTest:
         self.devices.append(self.server)
         result = []
         for dev in self.devices:
-            try:
-                proc = subprocess.Popen(
-                    ['netperf -H '+dev[0]+'  -l -10000 -TCP_RR  -b 1 -v 2 -- -O mean_latency'], stdout=subprocess.PIPE, shell=True)
-                outs, errs = proc.communicate(timeout=self.TIMEOUT)
-                lines = outs.splitlines()
-                if len(lines) == 7:
-                    latency = lines[6].rstrip().decode('utf-8')
-                    print('Device: ', dev[0], ' latency in usec: ', latency)
-                    result.append([dev[1], dev[2], latency])
-            except subprocess.TimeoutExpired as t:
-                self.logger.error("Timeout error when measuring latency to device mac:" + str(
-                    dev[1]) + " hostname:" + str(dev[2]) + " watchdog: " + str(self.TIMEOUT))
-            except Exception as e:
-                self.logger.error(
-                    "Error during latency measurement " + str(e))
+            result = None
+            # try:
+            #     proc = subprocess.Popen(
+            #         ['netperf -H '+dev[0]+'  -l -10000 -TCP_RR  -b 1 -v 2 -- -O mean_latency'], stdout=subprocess.PIPE, shell=True)
+            #     outs, errs = proc.communicate(timeout=self.TIMEOUT)
+            #     lines = outs.splitlines()
+            #     if len(lines) == 7:
+            #         latency = lines[6].rstrip().decode('utf-8')
+            #         print('Device: ', dev[0], ' latency in usec: ', latency)
+            #         result.append([dev[1], dev[2], latency])
+            # except subprocess.TimeoutExpired as t:
+            #     self.logger.error("Timeout error when measuring latency to device mac:" + str(
+            #         dev[1]) + " hostname:" + str(dev[2]) + " watchdog: " + str(self.TIMEOUT))
+            # except Exception as e:
+            #     self.logger.error(
+            #         "Error during latency measurement " + str(e))
         return result
 
     """
